212aac9a20e3e542b3de11dc833c39e0069a6cb5
Colin Slater <ctslater@uw.edu>
2019-10-17 15:18:28 -0400

    Add ParameterTracker to do bookkeeping.

---
+++ include/lsst/pipe/crowd/ParameterTracker.h
@@ -0,0 +1,37 @@
+
+#include <vector>
+#include <map>
+#include <tuple>
+
+namespace lsst {
+namespace pipe {
+namespace crowd {
+
+
+class ParameterTracker {
+public:
+    ParameterTracker(int nParameters);
+
+    void addSource(int sourceId);
+
+    int getPixelId(int pixelX, int pixelY);
+
+    int getSourceParameterId(int sourceId, int param);
+
+private:
+
+    int _nParameters;
+    int _nSources;
+    int _nPixels;
+
+    // image X,Y pixel -> matrix row
+    std::map<std::tuple<int, int>, int> _pixelMapping;
+
+    // sourceId, ParamId -> matrix column
+    std::map<std::tuple<int, int>, int> _sourceParameterMapping;
+
+};
+
+} // namespace crowd
+} // namespace pipe
+} // namespace lsst
---
+++ src/ParameterTracker.cc
@@ -0,0 +1,63 @@
+
+#include "lsst/pipe/crowd/ParameterTracker.h"
+// #include "lsst/pex/exceptions/Exception.h"
+#include "lsst/pex/exceptions/Runtime.h"
+
+#include <vector>
+#include <map>
+#include <tuple>
+
+namespace lsst {
+namespace pipe {
+namespace crowd {
+
+ParameterTracker::ParameterTracker(int nParameters) :
+    _nParameters(nParameters),
+    _pixelMapping(std::map<std::tuple<int, int>, int>()),
+    _sourceParameterMapping(std::map<std::tuple<int, int>, int>())
+{
+}
+
+void ParameterTracker::addSource(int sourceId) {
+
+    int nextParameterId;
+
+    for(int i = 0; i < _nParameters; i++) {
+        nextParameterId = _sourceParameterMapping.size();
+        _sourceParameterMapping.insert({std::make_tuple(sourceId, i),
+                                       nextParameterId});
+    }
+}
+
+/*
+ * getPixelId can mutate the mapping if a pixel isn't found,
+ * because we expect this to get built up before solving and
+ * then not need to be accessed again.
+ */
+int ParameterTracker::getPixelId(int pixelX, int pixelY) {
+
+    int newPixelId;
+    auto pixelMapEntry = _pixelMapping.find(std::make_tuple(pixelX, pixelY));
+
+    if(pixelMapEntry != _pixelMapping.end()) {
+        return pixelMapEntry->second;
+    } else {
+        newPixelId = _pixelMapping.size();
+        _pixelMapping.insert({std::make_tuple(pixelX, pixelY), newPixelId});
+        return newPixelId;
+    }
+
+}
+
+int ParameterTracker::getSourceParameterId(int sourceId, int param) {
+    auto result = _sourceParameterMapping.find(std::make_tuple(sourceId, param));
+    if(result == _sourceParameterMapping.end()) {
+        throw LSST_EXCEPT(lsst::pex::exceptions::RuntimeError, "Request for SourceParameter that does not exist.");
+    }
+    return result->second;
+}
+
+}
+}
+}
+
